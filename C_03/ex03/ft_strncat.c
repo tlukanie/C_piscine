7fb1a3b9a204a10345e3a7b56bc1114a6135921d
Tatiana <120662241+tlukanie@users.noreply.github.com>
2023-08-27 13:13:12 +0200

    C 03 + subj (75/100)

---
+++ C_03/ex03/ft_strncat.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlukanie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/21 11:40:40 by tlukanie          #+#    #+#             */
+/*   Updated: 2022/10/23 12:19:33 by tlukanie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int		i;
+	unsigned int		j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (j != nb && src[j] != '\0')
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
